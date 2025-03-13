dca7cd849bb1d8ddd0c2975af2222904a7011a08
Hamed <dekanosidzearchil@gmail.com>
2025-03-13 23:36:30 +0330

    before rendering of categories

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -7,26 +7,21 @@
 
 <script>
     function Ajax(url, data)
-        {
-            data =  $.extend(data, {"_token": "{{ csrf_token() }}"}); 
-            Myresult = ""
-            $.ajax({
-                method: "POST",
-                url: url,
-                data: data,
-                async: false,
-                success: function(result) {
-                    Myresult =  result
-                    }
-            })
-            return Myresult
-        }
-        $(document).ready(function() {
-            $('.errorFromController').delay(5000).fadeOut('slow');
-            $('.successFromController').delay(5000).fadeOut('slow');
-
-
-    })
+    {
+        data =  $.extend(data, {"_token": "{{ csrf_token() }}"}); 
+        Myresult = ""
+        $.ajax({
+            method: "POST",
+            url: url,
+            data: data,
+            async: false,
+            success: function(result) {
+                Myresult =  result
+                }
+        })
+        return Myresult
+    }
+    
     function MathBreak() {
         document.querySelectorAll('mstyle').forEach(el => {
             const newEl = document.createElement('p');
@@ -35,46 +30,13 @@
             el.replaceWith(newEl); // Replace the custom tag
         }); 
     }
+
+    $(document).ready(function() {
+        $('.errorFromController').delay(5000).fadeOut('slow');
+        $('.successFromController').delay(5000).fadeOut('slow');
+    })
 </script>
 
-{{-- <script type="text/javascript" src="{{asset('assets/MathJax.js')}}"></script>
-
-<script type="text/x-mathjax-config;executed=true">
-    MathJax.Hub.Config({
-    tex2jax: { inlineMath: [["$","$"],["\\(","\\)"]] },
-    "HTML-CSS": {
-    linebreaks: { automatic: false, width: "container" },
-    scale: 120,
-     mtextFontInherit: true
-    }
-    });
-
-</script> --}}
 
 
-<script>
-    window.MathJax = {
-      tex: {
-        inlineMath: [['$', '$'], ['\\(', '\\)']]
-      },
-      "HTML-CSS": {
-            linebreaks: { automatic: false, width: "container" },
-            scale: 120,
-            mtextFontInherit: true
-            }
-        });
-    };
-</script>
-{{-- <script async src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-    
-<script>
 
-MathJax.typesetPromise().then(function() {
-    document.querySelectorAll(".MathJax mtext").forEach((element) => {
-        element.style.direction = "rtl !important";
-        element.style.textAlign = "right";
-        element.style.whiteSpace = "nowrap";
-    });
-});
-</script> --}}

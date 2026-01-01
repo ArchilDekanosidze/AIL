9ffd4ab6129cee83a580c34a72eafac70be8de0e
Hamed <dekanosidzearchil@gmail.com>
2026-01-01 14:08:32 +0330

    gam be gam

--- resources/views/partials/gambeGams.blade.php
+++ resources/views/partials/gambeGams.blade.php
@@ -1,24 +1,29 @@
 @foreach($gams as $gam)
-    <a class="pa-book-info-item sp-flex-column" 
-       href="{{$gam->url}}" 
-       title="{{ $gam->title }}">
-       
-        <div class="book-info-item-top sp-flex-1">
-            <div class="book-info-item-descimg sp-overflow-hidden">
-                <img src="{{ $gam->url}}" 
-                     alt="{{ $gam->title }}" 
-                     class="sp-w sp-h">
+
+<a class="pa-book-info-item sp-flex-column"
+   href="{{ $gam->url }}"
+   target="_blank"
+   title="{{ $gam->title }}">
+
+    <div class="book-info-item-top sp-flex-1">
+
+        <div class="book-info-item-descimg sp-overflow-hidden">
+            <img src="{{ $gam->url }}"
+                 alt="{{ $gam->title }}"
+                 class="sp-w sp-h">
+        </div>
+
+        <div class="sp-flex-1 sp-d-flex sp-flex-column sp-justify-center sp-mr-4 sp-m-auto sp-ml-1">
+            <div class="book-info-item-text sp-mb-3">
+                <h2 class="sp-font-large-new2">{{ $gam->title }}</h2>
             </div>
 
-            <div class="sp-flex-1 sp-d-flex sp-flex-column sp-justify-center sp-mr-4 sp-m-auto sp-ml-1">
-                <div class="book-info-item-text sp-mb-3">
-                    <h2 class="sp-font-large-new2">{{ $gam->title }}</h2>
-                </div>
-
-                <div class="book-info-item-text-desc">
-                    <p>{{ $gam->description }}</p>
-                </div>
+            <div class="book-info-item-text-desc">
+                <p>{{ $gam->description }}</p>
             </div>
         </div>
-    </a>
+
+    </div>
+</a>
+
 @endforeach
